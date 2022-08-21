481e4693a6a42287e3bfd101e50f8488b8790669
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-08-21 17:05:43 +0300

    create login page

--- src/app/admin/login-page/login-page.component.ts
+++ src/app/admin/login-page/login-page.component.ts
@@ -1,15 +1,39 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {User} from "../../shared/interfaces";
 
 @Component({
-  selector: 'app-login-page',
-  templateUrl: './login-page.component.html',
-  styleUrls: ['./login-page.component.scss']
+    selector: 'app-login-page',
+    templateUrl: './login-page.component.html',
+    styleUrls: ['./login-page.component.scss']
 })
 export class LoginPageComponent implements OnInit {
 
-  constructor() { }
+    form!: FormGroup;
 
-  ngOnInit(): void {
-  }
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        this.form = new FormGroup({
+            email: new FormControl(null, [
+                Validators.email,
+                Validators.required]),
+            password: new FormControl(null, [
+                Validators.required, Validators.minLength(6)
+            ]),
+        })
+    }
+    submit(){
+        console.log(this.form)
+        if(this.form.invalid){
+            return
+        }
+        const user: User = {
+            email: this.form.value.email,
+            password: this.form.value.password
+
+        }
+    }
 
 }
