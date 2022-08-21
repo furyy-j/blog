9e13fc802fbdf8d3ea960770f6e6ae3056ad2773
furyy-j <96385920+furyy-j@users.noreply.github.com>
2022-08-22 02:05:15 +0300

    create alert service

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -10,6 +10,8 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {SharedModule} from "../shared/shared.module";
 import {AuthGuard} from "./shared/services/auth.guard";
 import {SearchPipe} from "./shared/search.pipe";
+import { AlertComponent } from './shared/components/alert/alert.component';
+import {AlertService} from "./shared/services/alert.service";
 
 
 const routes: Routes = [
@@ -29,7 +31,8 @@ const routes: Routes = [
         DashboardPageComponent,
         CreatePageComponent,
         EditPageComponent,
-        SearchPipe
+        SearchPipe,
+        AlertComponent
     ],
     imports: [
         CommonModule,
@@ -40,7 +43,7 @@ const routes: Routes = [
 
     ],
     exports: [RouterModule],
-    providers: [AuthGuard]
+    providers: [AuthGuard,AlertService]
 })
 
 export class AdminModule {
--- src/app/admin/edit-page/edit-page.component.ts
+++ src/app/admin/edit-page/edit-page.component.ts
@@ -1,15 +1,55 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {ActivatedRoute, Params} from "@angular/router";
+import {PostService} from "../../shared/post.service";
+import {Subscription, switchMap} from "rxjs";
+import {Post} from "../../shared/interfaces";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
+import {AlertService} from "../shared/services/alert.service";
 
 @Component({
   selector: 'app-edit-page',
   templateUrl: './edit-page.component.html',
   styleUrls: ['./edit-page.component.scss']
 })
-export class EditPageComponent implements OnInit {
+export class EditPageComponent implements OnInit, OnDestroy{
 
-  constructor() { }
+  form: FormGroup
+  post: Post
+  submitted = false
+  uSub: Subscription
+
+  constructor(private route: ActivatedRoute,
+              private postService: PostService,
+              private alert: AlertService) { }
 
   ngOnInit(): void {
+    this.route.params
+        .pipe(switchMap((params:Params)=>{
+          return this.postService.getById(params['id'])
+            })
+        ).subscribe((post:Post)=>{
+          this.post = post;
+          this.form = new FormGroup({
+            title: new FormControl(post.title, Validators.required),
+            text: new FormControl(post.text,Validators.required)
+          })
+    })
   }
-
+  submit(){
+    if(this.form.invalid){
+      return
+    }
+    this.submitted = true;
+    this.uSub = this.postService.update({
+      ...this.post,
+      text: this.form.value.text,
+      title: this.form.value.title,
+    }).subscribe(()=>{
+      this.submitted = false
+      this.alert.success('Пост був оновлений')
+    })
+  }
+  ngOnDestroy() {
+    if(this.uSub){this.uSub.unsubscribe()}
+  }
 }
